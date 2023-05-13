eebdb6e71dfc75da065fb26510bf23e97d9c4764
Magusest <redikor49@gmail.com>
2023-05-13 20:27:41 +0300

    Add advanages block

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initTabs} from './modules/tabs/init-tabs';
+import {getTrainerSlider} from './modules/sliders/trainers-slider';
 
 // ---------------------------------
 
@@ -20,6 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     initModals();
     initTabs();
+    getTrainerSlider();
     const form = new Form();
     window.form = form;
     form.init();
---
+++ source/js/modules/sliders/trainers-slider.js
@@ -0,0 +1,29 @@
+import Swiper from '../../vendor/swiper';
+
+export function getTrainerSlider() {
+  const slider = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  return slider;
+}
